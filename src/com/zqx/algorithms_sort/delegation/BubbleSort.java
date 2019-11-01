67baca99404ed46062c0428f49a5e395c7877ea9
zhanxiaoqing <zhanxiaoqing@didichuxing.com>
2019-11-01 09:54:05 +0800

    update project structure

---
+++ src/com/zqx/algorithms_sort/delegation/BubbleSort.java
@@ -0,0 +1,18 @@
+package com.zqx.algorithms_sort.delegation;
+
+public class BubbleSort {
+
+	public int[] sort(int[] arr) {
+		 for(int i=0;i<arr.length-1;i++)
+	      {
+	        for(int j=0;j<arr.length-1-i;j++){
+	          if(arr[j]>arr[j+1]){
+	          int temp = arr[j];
+	          arr[j] = arr[j+1];
+	          arr[j+1] = temp;
+	          }
+	        }
+	     }
+		 return arr;
+	}
+}
