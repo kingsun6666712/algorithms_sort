4cb3cfe66e9dfa6fb14d39b3c4dc8e6a45fa2aaa
kingsun6666712 <50617005+kingsun6666712@users.noreply.github.com>
2019-11-01 09:25:07 +0800

    only commit java source code
    aaa

---
+++ BubbleSort/src/com/zqx/algorithms_sort/Utils/PrintUtil.java
@@ -0,0 +1,14 @@
+package com.zqx.algorithms_sort.Utils;
+
+public class PrintUtil {
+	
+	public void print(int arr[]) {
+		
+		System.out.println("ÅÅĞòÊı×éÎª£º");
+		for(int num:arr) {
+			System.out.println(num+" ");
+		}
+		
+	}
+
+}
---
+++ BubbleSort/src/com/zqx/algorithms_sort/delegation/InsertSort.java
@@ -0,0 +1,20 @@
+package com.zqx.algorithms_sort.delegation;
+
+public class InsertSort {
+
+	public int[] insert(int[] arr) {
+		
+		int length=arr.length;
+		int insertNum;
+		for(int i=1;i<=length;i++) {
+			insertNum=arr[i];
+			int j = i-1;
+			while(j>=0&&arr[j]>insertNum) {
+				arr[j+1] = arr[j];
+				j--;
+			}
+			arr[i+1]=insertNum;
+		}
+		return arr;
+	}
+}
