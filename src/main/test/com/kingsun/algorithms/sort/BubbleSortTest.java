a1e1ae55a76277fda2ef7f0449b8c7eb565c43c8
kingsun6666712 <15011576124@139.com>
2019-11-19 11:03:11 +0800

    update and format

--- src/main/test/com/kingsun/algorithms/sort/BubbleSortTest.java
+++ src/main/test/com/kingsun/algorithms/sort/BubbleSortTest.java
@@ -8,28 +8,17 @@ import static org.junit.Assert.assertTrue;
 public class BubbleSortTest {
 
     @Test
-    public void test (){
-    	
-      BubbleSort sort = new BubbleSort();
-    
-      int numbers[] = {7,3,19,6,28,15,2,17};
+    public void test() {
 
-      int[] results =sort.sort(numbers);
+        BubbleSort sort = new BubbleSort();
 
-        assertEquals(results[0],2);
-        assertEquals(results[1],3);
-        assertEquals(results[2],6);
-        assertEquals(results[3],7);
+        int numbers[] = {7, 3, 19, 6, 28, 15, 2, 17};
 
-        assertEquals(results[4],15);
-        assertEquals(results[5],17);
-        assertEquals(results[6],19);
-        assertEquals(results[7],28);
+        int[] results = sort.sort(numbers);
 
-        for(int i=0;i<results.length-1;i++) {
-            assertTrue(results[i]<results[i+1]);
+        for (int i = 0; i < results.length - 1; i++) {
+            assertTrue(results[i] < results[i + 1]);
         }
-
 
 
     }
